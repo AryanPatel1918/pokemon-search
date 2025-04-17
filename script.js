37bc5a589e950b14c69b2d80c88fd9b150d19481
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-04-17 18:20:59 -0400

    Add working Pokémon search functionality using PokéAPI

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-
-fetchData();
-
 async function fetchData() {
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+
+        const pokemonName = document.getElementById("pokemonName").value.trim().toLowerCase();
+
+        if (!pokemonName) {
+            alert("Please enter a Pokémon name!");
+            return;
+        }
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if (!response.ok) {
             throw new Error("Could not fetch resource");
         }
         const data = await response.json();
-        console.log(data.weight);
+        const pokemonSprite = data.sprites.front_default;
+        
+        pokemonImgElement = document.getElementById("pokemonSprite");
+        pokemonImgElement.src = pokemonSprite;
+        pokemonImgElement.style.display = "block";
 
     } catch(error) {
         console.error(error);
