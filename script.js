49d761030b8883c416a9915576328e43f90758cc
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-04-17 18:45:17 -0400

    Hide image when Pokémon not found and show alert

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 async function fetchData() {
+
+    const pokemonImgElement = document.getElementById("pokemonSprite");
+    const pokemonName = document.getElementById("pokemonName").value.trim().toLowerCase();
+
+    if (!pokemonName) {
+        alert("Please enter a Pokémon name!");
+        pokemonImgElement.style.display = "none";
+        return;
+    }
+
     try {
-
-        const pokemonName = document.getElementById("pokemonName").value.trim().toLowerCase();
-
-        if (!pokemonName) {
-            alert("Please enter a Pokémon name!");
-            return;
-        }
-
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if (!response.ok) {
             throw new Error("Could not fetch resource");
         }
+
         const data = await response.json();
         const pokemonSprite = data.sprites.front_default;
         
-        pokemonImgElement = document.getElementById("pokemonSprite");
         pokemonImgElement.src = pokemonSprite;
         pokemonImgElement.style.display = "block";
 
     } catch(error) {
         console.error(error);
+        pokemonImgElement.style.display = "none";
+        alert("Pokémon not found! Please try again.");
     }
 }
